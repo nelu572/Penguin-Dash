2187c5548dba6a3a4851facd9941c03bfb9f1eb6
nelu572 <s25031@gsm.hs.kr>
2025-10-12 15:22:01 +0900

    임시타일 생성
    간단한 벽이랑, 얼음바닥만 있는 타일팔레트 생성함

--- Assets/Script/Player/PlayerMove.cs
+++ Assets/Script/Player/PlayerMove.cs
@@ -7,18 +7,18 @@ public class PlayerMove : MonoBehaviour
     [SerializeField] private float speed = 7;
 
     private Vector2 dir;
-    private bool isMoving = true;
+    private bool isMoving = false;
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
-        dir = Vector2.right; // 초기 이동 방향
+        dir = Vector2.zero;
     }
 
     private void Update()
     {
+        //Debug.Log(isMoving);
         if (!isMoving)
         {
-            // 멈춘 상태에서만 입력 받기
             Vector2 input = Vector2.zero;
             if (Input.GetKey(KeyCode.W)) input.y += 1;
             if (Input.GetKey(KeyCode.S)) input.y -= 1;
@@ -29,7 +29,7 @@ public class PlayerMove : MonoBehaviour
             {
                 input.Normalize();
                 dir = input;
-                isMoving = true; // 입력이 들어오면 다시 미끄러지기 시작
+                isMoving = true;
             }
         }
     }
@@ -48,10 +48,9 @@ public class PlayerMove : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        // 벽 레이어 감지
         if (((1 << collision.gameObject.layer) & wallLayer) != 0)
         {
-            isMoving = false; // 벽에 닿으면 멈춤
+            isMoving = false;
             rb.linearVelocity = Vector2.zero;
         }
     }
