55b4f67fb892c61eff361891fd181953e03e87fd
nelu572 <s25031@gsm.hs.kr>
2025-10-13 21:09:33 +0900

    플레이어 idle, walk 애니메이션 구현

--- Assets/Script/Player/PlayerMove.cs
+++ Assets/Script/Player/PlayerMove.cs
@@ -2,35 +2,47 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
+    private Animator anima;
+
     [SerializeField] private LayerMask wallLayer;
     private Rigidbody2D rb;
     [SerializeField] private float speed = 7;
 
     private Vector2 dir;
     private bool isMoving = false;
+
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         dir = Vector2.zero;
+        anima = GetComponent<Animator>();
     }
 
     private void Update()
     {
-        //Debug.Log(isMoving);
-        if (!isMoving)
+        if (rb.linearVelocity == Vector2.zero)
         {
+            anima.SetBool("Walk", false);
+
             Vector2 input = Vector2.zero;
             if (Input.GetKey(KeyCode.W)) input.y += 1;
             if (Input.GetKey(KeyCode.S)) input.y -= 1;
             if (Input.GetKey(KeyCode.A)) input.x -= 1;
             if (Input.GetKey(KeyCode.D)) input.x += 1;
-
+            if (input.x != 0)
+            {
+                transform.localScale = new Vector3(input.x * -1, 1, 1);
+            }
             if (input != Vector2.zero)
             {
                 input.Normalize();
                 dir = input;
                 isMoving = true;
             }
+        }
+        else
+        {
+            anima.SetBool("Walk", true);
         }
     }
 
@@ -48,6 +60,7 @@ public class PlayerMove : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        Debug.Log("충돌");
         if (((1 << collision.gameObject.layer) & wallLayer) != 0)
         {
             isMoving = false;
